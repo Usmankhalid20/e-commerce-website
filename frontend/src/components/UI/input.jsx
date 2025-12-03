c014aa9a1bfd3ce8f16c6148580543bc63c85adc
Usmankhalid20 <usmankhalid399354@gmail.com>
2025-12-03 22:50:07 +0500

    add input

---
+++ frontend/src/components/UI/input.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const input = () => {
+  return (
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere asperiores quod ea culpa molestias blanditiis ab iste veritatis quam consequatur rem ut, molestiae illo dolor autem dolore ipsam amet fugit repudiandae quia repellat neque tempore cum. Blanditiis, rem! Numquam sequi, id facere atque culpa eum velit consectetur magni laboriosam animi accusantium soluta quia inventore enim nobis, sit officia sapiente expedita asperiores ipsum saepe quae minus, ex facilis. Praesentium quae doloribus voluptatibus expedita dolorem temporibus quo provident error nihil explicabo odit repellendus est neque magnam iste, debitis, corrupti cum autem voluptatem laudantium voluptates fuga. Assumenda magni, consequatur porro animi ipsum officiis.
+    </div>
+  )
+}
